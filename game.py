fe8b36eb05ebd41e4eb93e559f28d976c8c6d90f
Coconinokid53 <bigdg2014@hotmail.com>
2023-10-04 15:19:37 -0600

    Module # 1 changes

--- game.py
+++ game.py
@@ -1,143 +1,82 @@
-import pygame
-pygame.init()
+print('Welcome to Suspense Quiz')
+answer = input('Are you ready to play the Quiz ? (yes/no) :')
+score = 0
+total_questions=7
 
-Width , Height = 600, 500
-Win = pygame.display.set_mode((Width, Height))
-pygame.display.set_caption("Pong")
-
-FPS = 60
-
-RED = (255, 255, 255)
-BLACK = (0, 0, 0)
-
-def draw(win):
-    PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
-    BALL_RADIUS = 7
-    win.fill(BLACK)
-class Paddle:
-    COLOR = RED
-    vel = 4
-
-
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-
-    def draw(self, WIN):
-        pygame.draw.rect()
-        pygame.draw.rectangle(win, self.COLOR, (self.x, self.y, self.width. self.height))
-
-    pygame.display.update()
-    if up:
-        self.y -= self.VeL
+# The idea that helped me come with this adventure game of loving watch suspense movies
+if answer.lower()=='yes':
+    answer=input('Question 1: you have two paths one rocky and one smooth which do you choose?')
+    if answer.lower()=='rocky':
+        score+=1
+        print('correct you found your dream car with the keys in the ignition full of gas')
     else:
-        def move(self, up=TRUE):
-            self.y +=self.VEL
-
-class Ball:
-    MAX_VEL = 5
-    COLOR = GREEN
-
-    def_init_(self, x, y, radius):
-    self.x = x
-    self.y = y
-    self.radius = radius
-    self.x_vel = MAX_VEL
-    self.y_vel = 0
-
-    def draw(self, win):
-        pygame.draw.circle(win, self.COLOR,(self.x, self.y), self.radius)
-
-    def move(self):
-        self.x += self.x_vel
-        self.y += self.y_vel
+        print('Wrong answer now you arrived at broken down house')
 
 
 
-def draw(win,paddles, ball):
-    win.fill(BLACK)
-
-    for paddle in paddles:
-        paddle.draw(win)
-    for  i in range(10, Height, Height//20):
-         if i % 2==1:
-            continue
-        pygame.draw.rect(win, RED, (WIDTH//2 - 5, i, 10, Height//20 ))
-    ball.draw(win)
-    pygame.display.update()
-
-
-def handle_collison(ball, left_paddle, right_Paddle):
-    if ball.y + ball.radius >= HEIGHT:
-        ball.y_vel *= -1
-    elif ball.y - ball.radius <= 0:
-        ball.y_vel *= -1
-
-
-    if ball.x_vel < 0:
-        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
-            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
-                ball.x_vel *= -1
-
-                middle_y = left_paddle.y + left_paddle.height / 2
-                difference_in_y = middle_y - ball.y
-                reduction_principle = (left_paddle.height / 2) / ball.MAX_VEL
-                y_vel = difference_in_y / reduction_principle
-                ball.y_vel = y_vel
+        answer=input('Question 2: You drive off to paradise or investigate the broken down house?')
+    if answer.lower()=='paradise':
+        score+=1
+        print('correct you investigate the house fall down unconcious and awake to find yourself in your bed')
     else:
-        if ball.y >= right_paddle.y and ball.x <= right_paddle.y + right_paddle.height:
-            if ball.x + ball.radius >= right_paddle.x:
-                ball.x_vel *= -1
-                
-                middle_y = right_paddle.y + right_paddle.height / 2
-                difference_in_y = middle_y - ball.y
-                reduction_principle = (right_paddle.height / 2) / ball.MAX_VEL
-                y_vel = difference_in_y / reduction_principle
-                ball.y_vel = y_vel
-
-
-def handle_paddle_movement(keys, left_Paddle, right_paddle):
-    if keys[pygame.K_w] and  left_Paddle.y - left_Paddle.VEL >= 0:
-        left_Paddle.move(up=True)
-    if keys[pygame.K_s] and left_Paddle.y + left_Paddle.VEL + left_Paddle.height <= HEIGHT:
-        left_Paddle.move(up=False)
-
-     if keys[pygame.K_UP]: and right_Paddle.y - right_Paddle.VEL >= 0:
-        right_Paddle.move(up=True)
-    if keys[pygame.K_DOWN]:and right_Paddle.y + right_Paddle.VEL + right_Paddle.height <= HEIGHT:
-        right_Paddle.move(up=False)
-
-    
+        print('Wrong you get a flat tire and arrive at a shady hotel and head on in')
 
 
 
 
-def main():
-    run = True
-    clock = pygame.time.Clock()
+        answer=input('Question 3: Look around your suroundings what do you see and hear in both endings?')
+    if answer.lower()=='Happy':
+        score+=1
+        print('correct you found it was all a nightmare with the abandoned house and awake happy')
+    else:
+        print('wrong you get checked in and told after trying to leave Welcome to the Hotel California')
 
-    
-    ball = Ball(WIDTH // 2, HEIGHT // 2, BALL_RADIUS)
-    
-    While run:
-        clock.tick(FPS)
-        draw(WIN [left_Paddle, right_paddle], ball)
 
-        for event in pygame.event.get():
-            left_Paddle = paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2)
-            right_paddle = paddle(width - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH)
-            if event.type == pygame.Quit:
-                run = False
-                break
 
-        keys = pygame.key.get_pressed()
-        handle_paddle_movement(keys, left_paddle, right_paddle)
+        answer=input('question 4: Welcome to the new suspense stuck in haunted house or locked in costco')
+    if answer.lower()=='Haunted':
+        score+=1
+        print ('Correct you get locked in costco having free range of snacks')
+    else:
+        print('wrong locked in haunted house complete darkness and unexpected jumpscares every hour')
 
-        ball.move()
-        handle_collison(ball, left_Paddle, right_paddle)
-    pygame.quit()
+        answer=input('Question 5: in costco you hear things do you investigate or found light and escape haunted house?')
+    if answer.lower()=='escape':
+        score+=1
+        print('correct you found an escape head back home and have a good nights rest')
+    else:
+        print('wrong hearing footsteps into the night and you go to hide in the freezer but the door shuts')
 
-if __name__ == '_main_':
-    main()
+
+
+        answer=input('question 6: go through the hole in the wall and escape or unexpectedly awake terrified?')
+    if answer.lower()=='Terrified':
+        score+=1
+        print('correct you found yourself not in the right house to actually sweat to find in another haunted house')
+    else:
+        print('wrong you escaped the freezer to hear freeze and end up in the back of a cop car for tresspassing')
+
+
+
+
+        answer=input('question 7: after heading to jail cop car is abdcuted you escape or call your parents and head home?')
+        if answer.lower()=='abducted':
+            score+=1
+        print('correct you came to find out it was all a nightmare')
+else:
+    print('wrong you cant seem to make sense if it was real or not')
+
+
+
+
+
+
+    # different set of code that was difficult to continue the adventure game
+    print('Thank you for playing my goblins and ghouls you attempted',score,'questions correctly!')
+    mark=(score/total_questions)*100
+    print('Marks obtained:',mark)
+    print('Happy Halloween')
+
+
+
+  # make a new situation to every story just make it more unpredicatble like life
